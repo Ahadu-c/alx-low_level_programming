cadbf7fb1b34a94d803c43a47315969adf611351
Ahadu-c <ahadu_boss@yahoo.com>
2022-09-08 11:15:25 -0700

    mandatory task correction 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
-#inlude <studio.h>
-main()
+#include <studio.h>
+/**
+ * main -entry point
+ *
+ * Return: 0 Always 0 (Sucesss)
+ */
+int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return 0
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
