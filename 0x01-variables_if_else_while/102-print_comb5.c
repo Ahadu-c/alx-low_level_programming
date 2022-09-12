3139f6048b2a75eacda4d586dbdb2be63d026e11
Ahadu-c <ahadu_boss@yahoo.com>
2022-09-12 03:30:52 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,25 +6,19 @@
  */
 int main(void)
 {
-	int i, p;
+	int i;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (p = 0; p < 100; j++)
 		{
-			if (p > i)
-			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
 				if (i != 98)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}
 		}
 	}
 	putchar('\n');
