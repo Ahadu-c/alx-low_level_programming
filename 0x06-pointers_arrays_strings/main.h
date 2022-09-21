53194e197595d3f0de2032aca600d809d1e630aa
Ahadu-c <ahadu_boss@yahoo.com>
2022-09-21 14:01:36 -0700

    Mandatory TAsks

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 #include <stdio.h>
 /**
- * void prototypes()
- * int prototypes()
- * char prototypes()
+ * void: prototypes()
+ * int: prototypes()
+ * char: prototypes()
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
@@ -13,12 +13,11 @@ int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-char *leet(char *);
-char *rot13(char *);
-char *indefinite_add(char *n1, char *r, intsize_r);
-char *add_strings(char *n1, char *n2, char *r, intr_index);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *indefinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif/*MAIN_H*/
-
+#endif /*MAIN_H*/
