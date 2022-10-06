32decd5d3c312192407dd4fde42b4554a8f1c6fb
Ahadu-c <ahadu_boss@yahoo.com>
2022-10-06 11:13:36 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,8 +29,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size && (ptr != NULL))
 	{
-		nptr = malloc(new_size)
-			if (nptr == NUll)
+		nptr = malloc(new_size);
+			if (nptr == NULL)
 				return (nptr);
 		for (i = 0; i < old_size; i++)
 			nptr[i] = *((char *)ptr + 1);
