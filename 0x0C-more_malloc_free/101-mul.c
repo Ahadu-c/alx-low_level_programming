27d345c572896554523bf2ffac8a9809b1301d26
Ahadu-c <ahadu_boss@yahoo.com>
2022-10-07 00:21:33 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -117,9 +117,9 @@ int main(int argc, char *argv[])
 			_putchar(e[ti]);
 		exit(98);
 	}
-	for (1l = 0; argv[1][l1]; l1++)
+	for (l1 = 0; argv[1][l1]; l1++)
 		;
-	for (l2 = 0; argv[2][2]; l2++)
+	for (l2 = 0; argv[2][l2]; l2++)
 		;
 	ln = l1 + l2 + 1;
 	a = malloc(ln * sizeof(char));
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti --, i++)
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
 		if (t == NULL)
