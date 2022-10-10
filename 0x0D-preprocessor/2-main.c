3d27140d05c74e9ba119a6ad6a45f1b21912c1ff
Ahadu-c <ahadu_boss@yahoo.com>
2022-10-10 04:15:13 -0700

    ALL TASKS

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib>
+#include <stdio.h>
+/**
+ * main - prints a name of file
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABSO
+#define ABSO
+
+#define ABS(X) (((x) < (0)) ? (-(x)) : (x))
+
+#endif /* ABS(X) */
