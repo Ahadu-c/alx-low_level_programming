4725410c3d9544131f56f28026f2cd66aabe1295
Ahadu-c <ahadu_boss@yahoo.com>
2022-10-13 09:16:30 -0700

    most tasks

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_c - print a char
+ * @c: char to print
+ *
+ * Return: vod
+ */
+void print_c(va_ist c)
+{
+
