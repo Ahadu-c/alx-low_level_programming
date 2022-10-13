6c6ce6ef1eb968fc45dbb894b97956ba4a956357
Ahadu-c <ahadu_boss@yahoo.com>
2022-10-13 11:12:50 -0700

    Task 02

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (separator == NULL)
 		separator = "";
 
-	va_start(strings, n)
+	va_start(strings, n);
 
 		for (i = 0; i < n; i++)
 		{
