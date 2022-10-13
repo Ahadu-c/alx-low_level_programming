7b121085bf18205369c95f98db697bd88103ae6d
Ahadu-c <ahadu_boss@yahoo.com>
2022-10-13 09:21:56 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
-#include <stardrg.h>
+#include <stdarg.h>
 /**
  * struct print - print type with corresponding print function
  * @t: print type
