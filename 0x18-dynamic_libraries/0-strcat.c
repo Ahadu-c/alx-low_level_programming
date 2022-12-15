24192ce990e7c4bb5e036a8e25d6927b1a1722e3
Ahadu-c <ahadu_boss@yahoo.com>
2022-12-15 06:44:39 -0800

    Task 0

--- 0x18-dynamic_libraries/0-strcat.c
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -1,18 +1,25 @@
 #include "main.h"
-/**
- * _strcat-concatenates the string pointed by @src to
- * the end of string pointed to by @dest
- * @dest:String that will be appended
- * @src:String to be concatenated upon
- * Return: returns pointer to @dest
- */
-char *_strcat(char *dest, char *src);
-{
-	int index = 0, dest_len = 0;
 
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+/**
+ * _strcat - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
